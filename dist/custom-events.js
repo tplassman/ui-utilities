de918b1d039cf91a02bc5d68200940a8feb414df
Daron Adkins <daron.adkins@gmail.com>
2019-09-30 14:44:51 -0400

    add event listener wrapper to custom events (#1)
    * add event listener wrapper to custom events
    
    * update version in package.json

--- dist/custom-events.js
+++ dist/custom-events.js
@@ -4,18 +4,30 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.emit = emit;
+exports.on = on;
 
 /**
  * Emit event - wrapper around CustomEvent API
  * @param {string} eventHandle
  * @param {object} eventDetails
+ * @param {EventTarget} target
  */
-
-/* eslint-disable import/prefer-default-export */
 function emit(eventHandle, eventDetails) {
+  var target = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : window;
   var event = new CustomEvent(eventHandle, {
     detail: eventDetails
   });
-  window.dispatchEvent(event);
+  target.dispatchEvent(event);
 }
-/* eslint-enable import/prefer-default-export */
+/**
+ * Listen for custom event and execute callback on EventTarget
+ * @param {string} eventHandle
+ * @param {function} cb
+ * @param {EventTarget} target
+ */
+
+
+function on(eventHandle, cb) {
+  var target = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : window;
+  target.addEventListener(eventHandle, cb);
+}
